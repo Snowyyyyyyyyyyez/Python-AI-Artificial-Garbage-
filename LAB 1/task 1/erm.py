69e5a7e1549ecb148ea8d6440a2def74a0bcab9b
Snowyyyyyyyyyyez <hassan2m14@gmail.com>
2026-01-20 13:54:27 +0300

    added files

---
+++ LAB 1/task 1/erm.py
@@ -0,0 +1,6 @@
+from func import multi
+print(multi(9, 7))
+from func import sub
+print(sub(9, 4))
+from func import add
+print(add(9, 18))
---
+++ LAB 1/task 1/func.py
@@ -0,0 +1,8 @@
+def multi(x, y):
+    return x * y
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
